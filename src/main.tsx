deeb0d9edf8d6832e48716dccd81547a38bbe88f
diegoromero <yolos12345>
2022-07-07 19:15:21 -0400

    Add Prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {MantineProvider} from "@mantine/core";
-import {NotificationsProvider} from "@mantine/notifications";
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
