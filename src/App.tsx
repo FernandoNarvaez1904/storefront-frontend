08ac1fce8d91092bbe603d0a5c3f67406b706eda
diegoromero <yolos12345>
2022-07-22 14:48:52 -0600

    Adding app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { AppShell } from '@mantine/core';
+import React from 'react';
 import Sidebar from './components/Sidebar';
+import './global.css';
 
 function App() {
-  return <Sidebar />;
+  return (
+    <AppShell padding="md" navbar={<Sidebar />}>
+      Hello
+    </AppShell>
+  );
 }
 
 export default App;
