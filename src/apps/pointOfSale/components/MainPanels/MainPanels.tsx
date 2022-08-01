a3b399a7a20fc138687d154ee92d0fb49644c095
diegoromero <yolos12345>
2022-08-01 15:04:17 -0600

    Main Panel styles

--- src/apps/pointOfSale/components/MainPanels/MainPanels.tsx
+++ src/apps/pointOfSale/components/MainPanels/MainPanels.tsx
@@ -1,34 +1,31 @@
-import "apps/pointOfSale/assets/Split.js.css";
-import React from "react";
-import { graphql, useLazyLoadQuery } from "react-relay";
-import Split from "react-split";
-import CurrentOrderPanel from "../CurrentOrderPanel";
-import ItemGridPanel from "../ItemGridPanel";
-import { MainPanels_ConfQuery } from "./__generated__/MainPanels_ConfQuery.graphql";
+import 'apps/pointOfSale/assets/Split.js.css';
+import React from 'react';
+import { graphql, useLazyLoadQuery } from 'react-relay';
+import Split from 'react-split';
+import CurrentOrderPanel from '../CurrentOrderPanel';
+import ItemGridPanel from '../ItemGridPanel';
+import { MainPanels_ConfQuery } from './__generated__/MainPanels_ConfQuery.graphql';
+import useMainPanelStyles from './MainPanelStyles';
 
 const confQuery = graphql`
-    query MainPanels_ConfQuery {
-        pointOfSaleConf {
-            gridType
-        }
+  query MainPanels_ConfQuery {
+    pointOfSaleConf {
+      gridType
     }
+  }
 `;
 
 function MainPanels() {
   const dataConf = useLazyLoadQuery<MainPanels_ConfQuery>(confQuery, {});
+  const { classes } = useMainPanelStyles();
+  const isImageGrid = dataConf.pointOfSaleConf.gridType === 'ImageGrid';
 
   return (
     <Split
-      style={{
-        display: "flex",
-        flexGrow: 1
-      }}
+      className={classes.root}
       sizes={[30, 70]}
       gutterSize={22}
-      minSize={[
-        450,
-        dataConf.pointOfSaleConf.gridType === "ImageGrid" ? 650 : 710
-      ]}
+      minSize={[450, isImageGrid ? 650 : 710]}
       gutterAlign="center"
     >
       <CurrentOrderPanel />
