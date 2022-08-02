b8a8d2c2c6d24af900056c56e057d5c6deb37ac7
diegoromero <yolos12345>
2022-08-02 15:16:13 -0600

    Add inventory to router and homepage grid
    #39ntf46[CLOSED]

--- src/routing/allRoutes.ts
+++ src/routing/allRoutes.ts
@@ -1,6 +1,7 @@
 import { MantineColor } from '@mantine/core';
+import Inventory from 'apps/inventory';
 import PointOfSale from 'apps/pointOfSale';
-import { Cash, Icon } from 'tabler-icons-react';
+import { BuildingWarehouse, Cash, Icon } from 'tabler-icons-react';
 
 export interface AppRoute {
   path: string;
@@ -17,6 +18,13 @@ const allRoutes: Array<AppRoute> = [
     color: 'green',
     icon: Cash,
     element: PointOfSale,
+  },
+  {
+    title: 'Inventory',
+    path: 'inventory',
+    color: 'red',
+    icon: BuildingWarehouse,
+    element: Inventory,
   },
 ];
 
