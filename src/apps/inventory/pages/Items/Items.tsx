4cd40a2d1943262dbd0aadaecd859b4467c1b557
Diego Romero <diego.romero@unleashtechnologies.net>
2022-08-14 00:49:08 -0600

    Created Paper where data will be displayed

--- src/apps/inventory/pages/Items/Items.tsx
+++ src/apps/inventory/pages/Items/Items.tsx
@@ -1,11 +1,11 @@
-import { Box, Stack } from '@mantine/core';
+import { Paper, Stack } from '@mantine/core';
 import ItemsHeader from './components/ItemsHeader';
 
 function Items() {
   return (
     <Stack sx={{ height: '100%' }}>
       <ItemsHeader />
-      <Box sx={{ flexGrow: 1 }}>rest</Box>
+      <Paper sx={{ flexGrow: 1 }} withBorder p="md" shadow="md" />
     </Stack>
   );
 }
