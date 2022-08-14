00c182d45ca59c6a04fc5d54a7f88c52dbccd3a8
Diego Romero <diego.romero@unleashtechnologies.net>
2022-08-13 23:35:50 -0600

    Stack as layout component for Inventory/Items. Using flexGrow to take all additional space.

--- src/apps/inventory/pages/Items/Items.tsx
+++ src/apps/inventory/pages/Items/Items.tsx
@@ -1,5 +1,12 @@
+import { Box, Stack } from '@mantine/core';
+
 function Items() {
-  return <h1>Items</h1>;
+  return (
+    <Stack sx={{ height: '100%' }}>
+      <Box>Header</Box>
+      <Box sx={{ flexGrow: 1 }}>rest</Box>
+    </Stack>
+  );
 }
 
 export default Items;
