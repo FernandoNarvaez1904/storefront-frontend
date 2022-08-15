8caee8d232a6f2e4340b0d90b316f48e0dc968e9
Diego Romero <diego.romero@unleashtechnologies.net>
2022-08-15 09:30:46 -0600

    Create Basic Header Layout

--- src/apps/inventory/Inventory.tsx
+++ src/apps/inventory/Inventory.tsx
@@ -1,12 +1,18 @@
-import { AppShell } from '@mantine/core';
+import { AppShell, Box, Stack } from '@mantine/core';
+import Header from 'apps/inventory/components/Header';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function Inventory() {
   return (
-    <AppShell navbar={<Sidebar />}>
-      {/* Renders the content inside Sub-Route */}
-      <Outlet />
+    <AppShell navbar={<Sidebar />} padding={0}>
+      <Stack sx={{ height: '100%' }}>
+        <Header />
+        <Box p="xs" sx={{ flexGrow: 1 }}>
+          {/* Renders the content inside Sub-Route */}
+          <Outlet />
+        </Box>
+      </Stack>
     </AppShell>
   );
 }
