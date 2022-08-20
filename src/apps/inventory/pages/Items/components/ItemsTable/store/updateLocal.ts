01815e28bee61900c5d59cc8bb69df5a5083d43f
Diego Romero <diego.romero@unleashtechnologies.net>
2022-08-19 20:53:40 -0600

    Create Local Store to handle Item Drawer

---
+++ src/apps/inventory/pages/Items/components/ItemsTable/store/updateLocal.ts
@@ -0,0 +1,29 @@
+import { commitLocalUpdate, RecordProxy } from 'relay-runtime';
+import RelayModernEnvironment from 'relay-runtime/lib/store/RelayModernEnvironment';
+
+export const openItemDrawer = (
+  environment: RelayModernEnvironment,
+  itemId: string
+) => {
+  commitLocalUpdate(environment, (store) => {
+    const item = store.get(itemId) as RecordProxy<{}>;
+
+    const drawerState = store.get(
+      'client:root:itemsTable_localState:drawerState'
+    );
+
+    drawerState?.setValue(true, 'opened');
+    drawerState?.setLinkedRecord(item, 'currentItem');
+  });
+};
+
+export const closeItemDrawer = (environment: RelayModernEnvironment) => {
+  commitLocalUpdate(environment, (store) => {
+    const drawerState = store.get(
+      'client:root:itemsTable_localState:drawerState'
+    );
+
+    drawerState?.setValue(false, 'opened');
+    drawerState?.setValue(null, 'currentItem');
+  });
+};
