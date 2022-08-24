3085f25636eb30473071c15faf1c2fe9e1d22aee
Diego Romero <diego.romero@unleashtechnologies.net>
2022-08-24 17:27:21 -0600

    Fixing type error

--- src/apps/inventory/pages/Items/state/atoms.ts
+++ src/apps/inventory/pages/Items/state/atoms.ts
@@ -1,8 +1,15 @@
 import { atom } from 'recoil';
 
+export const drawerStateAtom = atom({
+  key: 'drawerState',
+  default: {
+    isOpened: true,
+  },
+});
+
 export const itemsTableFilterAtom = atom<{
   type: 'name' | 'barcode' | 'sku';
-  value: '';
+  value: string;
 }>({
   key: 'itemsTableFilter',
   default: {
