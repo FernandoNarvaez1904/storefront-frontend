fb9389be8216988dea92991baee1bda3e22d0a15
Diego Romero <diego.romero@unleashtechnologies.net>
2022-10-21 02:22:10 -0600

    Added and configured most required libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
+import {
+  Button,
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from '@mantine/core';
+import { useLocalStorage } from '@mantine/hooks';
+import { NotificationsProvider } from '@mantine/notifications';
+import { RelayEnvironmentProvider } from 'react-relay';
+import relayEnvironment from './RelayEnvironment';
+
 function App() {
-  return <div>Hello world</div>;
+  return <Button>Hello</Button>;
 }
 
 function WrappedApp() {
-  return <App />;
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'colorScheme',
+    defaultValue: 'light',
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <NotificationsProvider position="top-right">
+          <RelayEnvironmentProvider environment={relayEnvironment}>
+            <App />
+          </RelayEnvironmentProvider>
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
+
 export default WrappedApp;
