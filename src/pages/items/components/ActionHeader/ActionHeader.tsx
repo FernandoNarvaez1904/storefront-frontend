d87f43624c0ce4700714adb4285722820480360d
Diego Romero <diego.romero@unleashtechnologies.net>
2022-10-27 18:59:29 -0600

    Using DefaultDrawer

--- src/pages/items/components/ActionHeader/ActionHeader.tsx
+++ src/pages/items/components/ActionHeader/ActionHeader.tsx
@@ -1,24 +1,19 @@
-import { useTheme } from '@emotion/react';
 import {
   Breadcrumbs,
-  Drawer,
   Group,
   MediaQuery,
   Text,
-  Title,
   UnstyledButton,
 } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
 import { IconPlus } from '@tabler/icons';
 import { useState } from 'react';
+import DefaultDrawer from '../../../../components/DefaultDrawer';
 import { AddItemButton } from './ActionHeader.styles';
 import FormCreateItem from './FormCreateItem';
 
 function ActionHeader() {
-  const theme = useTheme();
-  const isScreenSM = useMediaQuery(`(max-width: ${theme.breakpoints.xs}px)`);
-
   const [drawerOpened, setDrawerOpened] = useState(false);
+
   return (
     <>
       <Group sx={{ justifyContent: 'space-between' }}>
@@ -62,19 +57,9 @@ function ActionHeader() {
           </AddItemButton>
         </MediaQuery>
       </Group>
-      <Drawer
-        opened={drawerOpened}
-        title={<Title order={2}>Add Item</Title>}
-        size={isScreenSM ? 'md' : 'xl'}
-        padding="xl"
-        position="right"
-        overlayColor={
-          theme.colorScheme === 'dark'
-            ? theme.colors.dark[9]
-            : theme.colors.gray[2]
-        }
-        overlayOpacity={0.55}
-        overlayBlur={3}
+      <DefaultDrawer
+        isOpen={drawerOpened}
+        title="Add Item"
         onClose={() => setDrawerOpened(false)}
       >
         <FormCreateItem
@@ -82,7 +67,7 @@ function ActionHeader() {
             setDrawerOpened(false);
           }}
         />
-      </Drawer>
+      </DefaultDrawer>
     </>
   );
 }
